01e33645f8d39307805d9dad97d4339acca0e76c
lsm <liusenmiao@gmail.com>
2017-05-18 10:43:02 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2017 lsm.
+ */
+
+var debug = require('debug')
+var LOG_LEVEL = process.env.LOG_LEVEL || 'debug'
+var DEFAULT_LEVELS = ['fatal', 'error', 'warn', 'info', 'debug', 'trace']
+
+/**
+ * Export constructor with default settings.
+ * @type {Function}
+ */
+exports = module.exports = createLogger(LOG_LEVEL, DEFAULT_LEVELS)
+
+
+/**
+ * Export constructor creator for customized usage.
+ * @type {Function}
+ */
+exports.createLogger = createLogger
+
+
+/**
+ * The constructor creator.
+ * @param  {String} DEFAULT_LEVEL Default level of logging if not supplied.
+ * @param  {Array}  LEVELS        Levels of logging.
+ * @return {Logger}               The Logger constructor.
+ */
+function createLogger(DEFAULT_LEVEL, LEVELS) {
+  LEVELS = LEVELS || DEFAULT_LEVELS
+  return function Logger(namespace, level) {
+    level = level || DEFAULT_LEVEL
+    var levelIdx = LEVELS.indexOf(level)
+    var logger = {}
+
+    LEVELS.forEach(function(lv, idx) {
+      if (levelIdx >= idx) {
+        var ns = namespace ? (namespace + ':' + lv) : lv
+        var log = debug(ns)
+        logger[lv] = function() {
+          log.apply(null, arguments)
+          return logger
+        }
+      } else {
+        logger[lv] = function() {
+          return logger
+        }
+      }
+    })
+
+    return logger
+  }
+}
